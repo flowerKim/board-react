3f11d95999629c05dfa6c5ef0b1764686f9a2cd8
flowerKim <aotlf_@nate.com>
2023-09-07 12:19:56 +0900

    color red

---
+++ src/assets/js/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+      // light: main값을 통해 계산됨
+      // dark: main값을 통해 계산됨
+      // contrastText: main값을 통해 계산됨
+    },
+  },
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./assets/js/theme";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
